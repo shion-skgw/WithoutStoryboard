9954935e791b38b72b82e7fa3cdaf2ac6820844c
Shion Sukegawa <sean.skgw@gmail.com>
2020-05-30 14:53:49 +0900

    Initial Commit

---
+++ WithoutStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WithoutStoryboard
+//
+//  Created by Shion on 2020/05/30.
+//  Copyright © 2020 Shion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
