e1685fa75711da428472194b5af98f89e376dd98
Shion Sukegawa <sean.skgw@gmail.com>
2020-06-01 01:33:55 +0900

    commit

--- WithoutStoryboard/ViewController.swift
+++ WithoutStoryboard/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+	override func loadView() {
+		let view = View(frame: UIScreen.main.bounds)
+		view.backgroundColor = .lightGray
+		self.view = view
 	}
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
 
 }
 
+class View: UIView {
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		let subview = UIView(frame: .zero)
+		subview.backgroundColor = .cyan
+		self.addSubview(subview)
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError()
+	}
+
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		let statusBarFrame = UIApplication.shared.windows.first?.windowScene?.statusBarManager?.statusBarFrame ?? .zero
+		var subviewFrame = self.frame
+		subviewFrame.origin.y += statusBarFrame.height
+		subviewFrame.size.height -= statusBarFrame.height
+		subviews.first?.frame = subviewFrame
+	}
+
+}
